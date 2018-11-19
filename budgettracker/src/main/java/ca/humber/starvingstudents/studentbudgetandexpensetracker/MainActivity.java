04ec8f1ccf9ec08784e9ec5be4f8a574e45ac53f
Sean Danagher <Sean.T.Danagher@gmail.com>
2018-11-19 18:35:30 -0500

    Firebase dependencies have been updated in the app build.gradle

--- budgettracker/src/main/java/ca/humber/starvingstudents/studentbudgetandexpensetracker/MainActivity.java
+++ budgettracker/src/main/java/ca/humber/starvingstudents/studentbudgetandexpensetracker/MainActivity.java
@@ -16,6 +16,10 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.support.design.widget.BottomNavigationView;
 import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    
 
         toolbar = getSupportActionBar();
         toolbar.setTitle("Home");
